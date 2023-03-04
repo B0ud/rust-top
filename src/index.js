0b1267e14c240542aaacbf65b865f5af17d97400
B0ud <mehdi.boudart@gmail.com>
2023-03-04 17:29:48 +0100

    minimal javascript style show cpu usage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     let i = 0;
 
-    setInterval(() => {
+    setInterval(async () => {
+        let response = await fetch('/api/cpus');
+        if (response.status !== 200) {
+            throw new Error(`HTTP error! status: ${reponse.status}`);
+        }
+
+        let json = await response.json();
+
         i = i + 1;
-        document.body.textContent = `cycle ${i}`;
+        document.body.textContent = JSON.stringify(json, null, 2);
     }, 1000);
 });
 
