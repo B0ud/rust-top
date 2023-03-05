7e2a4ab8766eb3911c0e4b99d247f35b7ea72709
B0ud <mehdi.boudart@gmail.com>
2023-03-05 18:35:55 +0100

    Add some css

--- src/index.mjs
+++ src/index.mjs
@@ -4,23 +4,29 @@ import htm from "https://unpkg.com/htm?module";
 const html = htm.bind(h);
 
 function App(props) {
-    return html`
+  return html`
     <div>
       ${props.cpus.map((cpu) => {
-        return html` <div>${cpu.toFixed(2)}% usage</div> `;
-    })}
+    return html`<div class="bar">
+          <div class="bar-inner" style="width: ${cpu}%"></div>
+          <label>${cpu.toFixed(2)}%</label>
+        </div>`;
+  })}
     </div>
   `;
 }
 
 let i = 0;
 
-setInterval(async () => {
-    let response = await fetch("/api/cpus");
-    if (response.status !== 200) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
+let update = async () => {
+  let response = await fetch("/api/cpus");
+  if (response.status !== 200) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
 
-    let json = await response.json();
-    render(html`<${App} cpus=${json}></${App}>`, document.body);
-}, 1000);
+  let json = await response.json();
+  render(html`<${App} cpus=${json}></${App}>`, document.body);
+};
+
+update();
+setInterval(update, 200);
